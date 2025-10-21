d613fc1d843398269156eb1e9c38f435765a3d17
hugo <hugo031c@gmail.com>
2025-10-21 12:00:15 +0200

    [TP2] les énumérés

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,9 +1,13 @@
 package personnages;
 
+import objets.Equipement;
+
 public class Romain {
 
 	private String nom;
 	private int force;
+	private int nbEquipements;
+	private Equipement[] equipements;
 
 	private Boolean isInvariantVerified() {
 		return force >= 0;
@@ -12,6 +16,8 @@ public class Romain {
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
+		this.nbEquipements = 0;
+		this.equipements = new Equipement[2];
 		assert isInvariantVerified() : "Invariant a la creation";
 	}
 
@@ -47,20 +53,55 @@ public class Romain {
 		assert isInvariantVerified() : "Invariant a la reception d'un coup";
 	}
 
+	public void sEquiper(Equipement equipement) {
+		switch (nbEquipements) {
+		case 0: {
+			System.out.println("Le soldat " + getNom() + " s'équipe avec un " + equipement.getNom() + ".");
+			equipements[0] = equipement;
+			nbEquipements += 1;
+			break;
+		}
+		case 1: {
+			if (equipements[0] == equipement) {
+				System.out.println("Le soldat " + getNom() + " possède déja un " + equipement.getNom() + " !");
+			} else {
+				System.out.println("Le soldat " + getNom() + " s'équipe avec un " + equipement.getNom() + ".");
+				equipements[1] = equipement;
+				nbEquipements += 1;
+			}
+			break;
+		}
+		case 2: {
+			System.out.println("Le soldat " + getNom() + " est déjà bien protégé !");
+			break;
+		}
+		default:
+			throw new IllegalArgumentException("Unexpected value: " + equipement);
+		}
+	}
+
 	@Override
 	public String toString() {
 		return this.nom;
 	}
 
 	public static void main(String[] args) {
+
 		Romain Minus = new Romain("Minus", 6);
 		Minus.parler("Bonjour !!!!");
-		
-		Gaulois Asterix = new Gaulois("Astérix", 18);
-		
-		Asterix.frapper(Minus);
-		Asterix.frapper(Minus);
-		Asterix.frapper(Minus);
+
+		Equipement casque = Equipement.CASQUE;
+		Equipement bouclier = Equipement.BOUCLIER;
+
+		System.out.println("Équipement 1 : " + casque);
+		System.out.println("Équipement 2 : " + bouclier);
+
+		System.out.println();
+
+		Minus.sEquiper(casque);
+		Minus.sEquiper(casque);
+		Minus.sEquiper(bouclier);
+		Minus.sEquiper(casque);
 	}
 
 }
